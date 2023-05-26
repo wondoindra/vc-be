8d1cad826631e59c20bb6d336eb846f1274e33ec
Wondo Indra <wondoindra28@gmail.com>
2023-05-26 13:08:54 +0700

    add express app

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const user = require('./routes/user')
+const admin = require('./routes/admin')
+
+const PORT = process.env.PORT || 8080;
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/user', user)
+app.use('/admin', admin)
+
+app.use('/', (req, res) => {
+  res.send('<html><b>Signup API Frontpage</b></html>')
+})
+
+app.listen(PORT)
+console.log(`app running port ${PORT}`)
