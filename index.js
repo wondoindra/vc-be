76426a05a4b5832173015a4fefed475a0c3cabc4
Wondo Indra <wondoindra28@gmail.com>
2023-06-02 16:13:20 +0700

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const user = require('./routes/user')
 const admin = require('./routes/admin')
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
